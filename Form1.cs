da25305bf8af93d67d3d71e431e48dfbbd74e5fe
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-04-23 11:13:20 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace txtEdit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string textToSave = textBox1.Text;
+            File.WriteAllText("savedText.txt", textToSave);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font(textBox1.Font,textBox1.Font.Style ^ FontStyle.Bold);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style ^ FontStyle.Italic);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style ^ FontStyle.Underline);
+        }
+    }
+}
